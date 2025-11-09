18a4073b1e19afe358c121e46b384f0604e48aa0
Patrycja <plubowiecka07@gmail.com>
2025-11-09 17:13:51 +0100

    feat: company invitation model created

--- backend/companies/models.py
+++ backend/companies/models.py
@@ -1,25 +1,30 @@
 from django.db import models
 from django.conf import settings
+import uuid
+import datetime
+import django.utils
+
+STATUS_MAIN = 'main'
+STATUS_PARTENER = 'partner'
+STATUS_BASIC = 'basic'
+
+
+COMPANY_STATUS_CHOICES = [
+        (STATUS_MAIN, STATUS_MAIN),
+        (STATUS_PARTENER, STATUS_PARTENER),
+        (STATUS_BASIC, STATUS_BASIC),
+    ]
 
 # Create your models here.
 
 class Company(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    STATUS_MAIN = 'main'
-    STATUS_PARTENER = 'partner'
-    STATUS_BASIC = 'basic'
     email = models.EmailField()
     representative = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE, null=True) # czy email nie bedzie w User?
 
-    STATUS_CHOICES = [
-        (STATUS_MAIN, 'main'),
-        (STATUS_PARTENER, 'partner'),
-        (STATUS_BASIC, 'basic'),
-    ]
-
     status = models.CharField(
         max_length=10,
-        choices=STATUS_CHOICES,
+        choices=COMPANY_STATUS_CHOICES,
         default=STATUS_BASIC,
     )
     created_at = models.DateTimeField(auto_now_add = True)
@@ -28,4 +33,35 @@ class Company(models.Model):
     def __str__(self):
         return self.name
     
-     
+    
+def get_expiry_time():
+    return django.utils.timezone.now() + datetime.timedelta(days=7)
+
+class CompanyInvitation(models.Model):
+    email = models.EmailField
+    company_name = models.CharField(max_length=255)
+    token = models.UUIDField(default=uuid.uuid4, unique=True)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    expires_at = models.DateTimeField(blank=True, default=get_expiry_time())
+    is_accepted = models.BooleanField(default=False)
+
+    company_status = models.CharField(
+        max_length=10,
+        choices=COMPANY_STATUS_CHOICES,
+        default=STATUS_BASIC,
+    )
+
+    def is_expired(self):
+        return django.utils.timezone.now() > self.expires_at
+    
+    def get_invitation_status(self):
+        if (self.is_accepted):
+            return "accepted"
+        elif self.is_expired():
+            return "expired"
+        else:
+            return "not accepted"
+    
+    def __str__(self):
+        return f'Company name: {self.company} is {self.get_invitation_status}'
