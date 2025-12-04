dd2d300d1a97f83783108082f64c9c8f1dc0d460
Patrycja <plubowiecka07@gmail.com>
2025-12-04 12:10:33 +0100

    feat: added password reset

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('token/validate/', views.CurrentUserView.as_view(), name='token_validate'),
+    path('password-reset/', views.PasswordResetRequestView.as_view(), name='password_reset_request'),
+    path('password-reset/confirm/', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
 
